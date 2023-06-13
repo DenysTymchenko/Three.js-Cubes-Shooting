137cd2e8c5b75c8f290761b8df946c7900d99e49
Denys <tymchenko.denys@gmail.com>
2023-06-14 00:22:13 +0300

    cubes color changed

--- src/Cubes.js
+++ src/Cubes.js
@@ -6,7 +6,7 @@ import {
 import { scene } from './Scene';
 
 const geometry = new BoxGeometry(5, 5, 5);
-const material = new MeshStandardMaterial({ metalness: 0.3, roughness: 0.4, });
+const material = new MeshStandardMaterial({ color: '#d13610', metalness: 0.3, roughness: 0.4, });
 const height = 5;
 
 function generateCubes() {
@@ -16,7 +16,7 @@ function generateCubes() {
         // Because our boxes got height, it will be placed a little bit under the floor by default.
         // To fix that we need to find an offset to push our cubes up, by adding offset to y coordinate.
         const yOffset = geometry.parameters.height / 2;
-        
+
         const cube = new Mesh(geometry, material);
         cube.position.set(x,  y + yOffset, z);
         cube.receiveShadow = true;
