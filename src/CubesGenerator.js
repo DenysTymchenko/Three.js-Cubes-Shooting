8f9db83bad3e0bfe84e0ae92f682ca8839c8b091
Denys <tymchenko.denys@gmail.com>
2023-06-14 13:57:32 +0300

    cubes positioned correctly now

--- src/CubesGenerator.js
+++ src/CubesGenerator.js
@@ -6,37 +6,42 @@ import {
 import * as CANNON from 'cannon-es';
 import { scene } from './Scene';
 
-// Those array will be used to combine meshes position with bodies position 
+// Those arrays will be used to combine meshes' positions with bodies' positions
 const bodies = []; // CANNON bodies will be contained here
 const meshes = []; // Three.js meshes will be contained here
 
+// Cube parameters
+const cubeWidth = 1;
+const cubeHeight = 1;
+const cubeDepth = 3;
+
 // Physics world
 const world = new CANNON.World();
-const shape = new CANNON.Box(new CANNON.Vec3(5,5,5));
+const shape = new CANNON.Box(new CANNON.Vec3(cubeWidth, cubeHeight, cubeDepth));
 
 // Scene object
-const geometry = new BoxGeometry(5, 5, 5);
+const geometry = new BoxGeometry(cubeWidth, cubeHeight, cubeDepth);
 const material = new MeshStandardMaterial({ color: '#d13610', metalness: 0.3, roughness: 0.4, });
-const height = 5;
+const height = 3;
 
 function generateCubes() {
-  for (let y = height - 1; y >= 0; y--) {
-    for (let x = 0; x < 3; x++) {
-      for (let z = 0; z < 3; z++) {
-        // Because our boxes got height, it will be placed a little bit under the floor by default.
-        // To fix that we need to find an offset to push our cubes up, by adding offset to y coordinate.
-        const yOffset = geometry.parameters.height / 2;
+  for (let x = 0; x < 3; x++) {
+    for (let y = 0; y < (height) * cubeHeight; y+= cubeHeight) {
+      for (let z = 0; z === 0; z++) {
+        // Because our boxes have height, they will be placed a little bit under the floor by default.
+        // To fix that, we need to find an offset to push our cubes up by adding the offset to the y coordinate.
+        const yOffset = cubeHeight / 2;
 
         const cubeBody = new CANNON.Body({
           mass: 1,
-          position: new CANNON.Vec3(x,  y + yOffset, z),
+          position: new CANNON.Vec3(x, y + yOffset, z),
           shape,
         });
         bodies.push(cubeBody);
         world.addBody(cubeBody);
 
         const cubeMesh = new Mesh(geometry, material);
-        cubeMesh.position.set(x,  y + yOffset, z);
+        cubeMesh.position.set(x, y + yOffset, z);
         cubeMesh.receiveShadow = true;
         cubeMesh.castShadow = true;
         meshes.push(cubeMesh);
