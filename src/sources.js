d39ce4d82c74d0dae5b5879c2f04c7ec3fd44d8e
Denys <tymchenko.denys@gmail.com>
2023-06-20 00:18:10 +0300

    Firebase added. Bug with textures fixed

--- src/sources.js
+++ src/sources.js
@@ -4,48 +4,48 @@ export default [
     type: 'cubeTexture',
     path:
       [
-        'src/Textures/background/px.jpg',
-        'src/Textures/background/nx.jpg',
-        'src/Textures/background/py.jpg',
-        'src/Textures/background/ny.jpg',
-        'src/Textures/background/pz.jpg',
-        'src/Textures/background/nz.jpg'
+        '/Textures/background/px.jpg',
+        '/Textures/background/nx.jpg',
+        '/Textures/background/py.jpg',
+        '/Textures/background/ny.jpg',
+        '/Textures/background/pz.jpg',
+        '/Textures/background/nz.jpg'
       ]
   },
 
   {
     name: 'diamond-ore',
     type: 'texture',
-    path: 'src/Textures/diamond-ore.png',
+    path: '/Textures/diamond-ore.png',
   },
 
   {
     name: 'grassAmbientOcclusion',
     type: 'texture',
-    path: 'src/Textures/grass/ambientOcclusion.jpg',
+    path: '/Textures/grass/ambientOcclusion.jpg',
   },
 
   {
     name: 'grassColor',
     type: 'texture',
-    path: 'src/Textures/grass/color.jpg',
+    path: '/Textures/grass/color.jpg',
   },
 
   {
     name: 'grassNormal',
     type: 'texture',
-    path: 'src/Textures/grass/normal.jpg',
+    path: '/Textures/grass/normal.jpg',
   },
 
   {
     name: 'grassRoughness',
     type: 'texture',
-    path: 'src/Textures/grass/roughness.jpg',
+    path: '/Textures/grass/roughness.jpg',
   },
 
   {
     name: 'pickaxe-model',
     type: 'gltfModel',
-    path: 'src/Textures/pickaxe-model.glb'
+    path: '/Textures/pickaxe-model.glb'
   }
 ]
